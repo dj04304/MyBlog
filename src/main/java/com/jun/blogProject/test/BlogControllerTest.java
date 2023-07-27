49ef98d58e3c4f0389f40d3866577634f8e97379
dochi <dj043045@gmail.com>
2023-07-27 23:01:00 +0900

    setting

---
+++ src/main/java/com/jun/blogProject/test/BlogControllerTest.java
@@ -0,0 +1,13 @@
+package com.jun.blogProject.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //스프링이 특정 어노테이션이 붙어있는 클래스 파일을 new(IoC)해서 스프링 컨테이너에 관리해줍니다.
+public class BlogControllerTest {
+
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>hello test<h1>";
+	}
+}
